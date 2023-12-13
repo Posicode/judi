a7a8dc7c2472f34fc017c8de6dc3532ac956ddf2
Posicode <65912711+Posicode@users.noreply.github.com>
2023-12-13 22:41:07 +0100

    A lot

--- judi.js
+++ judi.js
@@ -1,5 +1,37 @@
+var roll = document.getElementById('roll');
+
 function firstclick() {
-    alert("hi");
-    // var body = document.getElementById('body');
-    // body.style.backgroundColor = "red";
+    var roll = document.getElementById('roll');
+    var computedStyle = window.getComputedStyle(roll);
+    var leftValue = parseInt(computedStyle.left);
+    document.getElementById('leftimg').src = 'material-symbols_arrow-circle-left.svg'
+    if (leftValue <= -1170) {
+        document.getElementById('rightimg').src = 'material-symbols_arrow-circle-right-outline-rounded.svg'
+    }
+    if (leftValue <= -2310) {
+        // document.getElementById('rightimg').src = 'material-symbols_arrow-circle-right-outline-rounded.svg'
+    }
+    else{
+        roll.style.left = leftValue - 810 + "px";
+    }
+}
+
+
+
+
+function secclick() {
+    var roll = document.getElementById('roll');
+    var computedStyle = window.getComputedStyle(roll);
+    var leftValue = parseInt(computedStyle.left, 10);
+    document.getElementById('rightimg').src = 'material-symbols_arrow-circle-right.svg'
+
+    if (leftValue >= -1140) {
+        document.getElementById('leftimg').src = 'material-symbols_arrow-circle-left-outline.svg'
+    }
+    if (leftValue >= 0) {
+        // document.getElementById('leftimg').src = 'material-symbols_arrow-circle-left.svg'
+    }
+    else{
+        roll.style.left = leftValue + 810 + "px";
+    }
 }
